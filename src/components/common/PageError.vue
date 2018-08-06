86282d10095597d1475eaffab96c8345bcb9d3ac
yuanxx <824264439@qq.com>
2018-08-06 13:59:52 +0800

    增加 底部导航栏 svg图标

---
+++ src/components/common/PageError.vue
@@ -0,0 +1,32 @@
+<template>
+  <!--页面错误-->
+  <div class="pageErrors">
+    <icon slot="icon" name="other/error" class="error-icon"></icon>
+    <h3>{{showMsg}}</h3>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['showMsg']
+  }
+</script>
+
+<style scoped>
+  .pageErrors {
+    width: 100vw;
+    height: 50vh;
+    margin-top: 20vh;
+  }
+
+  .error-icon {
+    width: 128px;
+    height: 128px;
+  }
+
+  h2 {
+    font-weight: normal;
+  }
+
+</style>
